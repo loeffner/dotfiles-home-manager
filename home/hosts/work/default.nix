1b34329f4be95f9542c20a7a5f706815ca83600f
Andreas Loesel <andreas.loesel@mvtec.com>
2026-08-21 07:53:52 +0200

    work: rotate GPG signing key

--- home/hosts/work/default.nix
+++ home/hosts/work/default.nix
@@ -22,7 +22,7 @@
     settings.user.email = "andreas.loesel@mvtec.com";
 
     signing = {
-      key = "9617B4071A0782ABF39642FC7A5328AA1A546BFB";
+      key = "139D28D04F7B80DC514FC1135E46167EC018E93A";
     };
 
     includes = [
